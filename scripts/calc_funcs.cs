af34e6f4d285ceb2a239cbe02eb45173efdd1794
shun-irie <82134201+shun-irie@users.noreply.github.com>
2024-07-14 14:13:46 +0900

    Update calc_funcs.cs

--- scripts/calc_funcs.cs
+++ scripts/calc_funcs.cs
@@ -85,7 +85,7 @@ namespace RPC
                     case "RightLowerLeg": return HumanBodyBones.RightLowerLeg;
                     case "RightFoot": return HumanBodyBones.RightFoot;
                     case "RightToes": return HumanBodyBones.RightToes;
-                    default: return HumanBodyBones.LastBone; // –¢‘Î‰ž‚Ìê‡
+                    default: return HumanBodyBones.LastBone; // 無効な場合の対処
                 }
             }
 
@@ -152,7 +152,7 @@ namespace RPC
                 }
             }
 
-            public static void UpdateAnimator(PhotonView photonView, Animator sourceAnimator, ref float timer, float timePerFrame, string rpcMethodName)
+            public static void UpdateAnimator(PhotonView photonView, Animator sourceAnimator, Transform root, ref float timer, float timePerFrame, string rpcMethodName)
             {
                 if (photonView.IsMine)
                 {
@@ -162,10 +162,37 @@ namespace RPC
                         timer -= timePerFrame;
                         if (sourceAnimator != null)
                         {
-                            photonView.RPC(rpcMethodName, RpcTarget.All, GetBoneRotationsAsByteArray(sourceAnimator));
+                            byte[] boneRotations = GetBoneRotationsAsByteArray(sourceAnimator);
+                            byte[] rootPosition = Vector3ToByteArray(root.position);
+                            byte[] rootRotation = QuaternionToByteArray(root.rotation);
+
+                            byte[] combinedData = new byte[boneRotations.Length + rootPosition.Length + rootRotation.Length];
+                            Buffer.BlockCopy(boneRotations, 0, combinedData, 0, boneRotations.Length);
+                            Buffer.BlockCopy(rootPosition, 0, combinedData, boneRotations.Length, rootPosition.Length);
+                            Buffer.BlockCopy(rootRotation, 0, combinedData, boneRotations.Length + rootPosition.Length, rootRotation.Length);
+
+                            photonView.RPC(rpcMethodName, RpcTarget.All, combinedData);
                         }
                     }
                 }
+            }
+
+            public static void SetAnimatorStateFromByteArray(Animator animator, Transform root, byte[] data)
+            {
+                if (!animator || !root) return;
+
+                int boneDataLength = (bones.Length * 16) + 12;
+                byte[] boneRotations = new byte[boneDataLength];
+                byte[] rootPosition = new byte[12];
+                byte[] rootRotation = new byte[16];
+
+                Buffer.BlockCopy(data, 0, boneRotations, 0, boneDataLength);
+                Buffer.BlockCopy(data, boneDataLength, rootPosition, 0, 12);
+                Buffer.BlockCopy(data, boneDataLength + 12, rootRotation, 0, 16);
+
+                SetBoneRotationsFromByteArray(animator, boneRotations);
+                root.position = ByteArrayToVector3(rootPosition, 0);
+                root.rotation = ByteArrayToQuaternion(rootRotation, 0);
             }
         }
     }
